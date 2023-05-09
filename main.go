5f61800bd6be32a9aae9ef9b42bac975bca23d94
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-05-09 07:42:59 +0100

    ..

--- main.go
+++ main.go
@@ -1,35 +1,57 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strconv"
+	"time"
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), "go-find by Nelson Isioma")
+		fmt.Fprintln(flag.CommandLine.Output(), "Copyright " + strconv.Itoa(time.Now().Local().Year()) + "\n")
+	}
+
+	flag.Parse()
 	args := os.Args[1:]
 
 	if len(args) > 1 {
 		fmt.Println("I can only find one item at a time")
 		os.Exit(1)
+
 	}
 
-	if err := run(args[0]); err != nil {
+	cmd := exec.Command("pwd")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	working_dir := out.String()
+
+	if err := run(working_dir, args[0]); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(fileName string) error {
+func run(working_dir, fileName string) error {
 	return filepath.WalkDir(".", func (path string, d os.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 		if d.Name() == fileName {
 			if d.IsDir() {
-				fmt.Println("found: " + path + d.Name())
+				fmt.Println("found: " + filepath.Join(working_dir, d.Name()))
 			} else {
-				fmt.Println("found: " + d.Name())
+				fmt.Println("found: " + filepath.Join(working_dir, d.Name()))
 			}
 		}
 		
